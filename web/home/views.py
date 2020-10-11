d86adda98634ae6dcd9409da5458d73de8bd3cac
Rohan saxena <rohaan.saxena14@gmail.com>
2020-10-11 17:54:20 +0530

    Update views.py

--- web/home/views.py
+++ web/home/views.py
@@ -1,28 +1,36 @@
 from django.shortcuts import render
 import pickle
 import sys
+from .scrapper import scrapper
 sys.path.append('C:\\Users\\Abhi\\Envs\\nlp\\lib\\site-packages')
 sys.path.append('C:\\Users\\Abhi\\Envs\\nlp')
 
-
 # Create your views here.
 def home(request):
-    return render(request,'home.html') 
+    return render(request,'index.html')
+
+def search(request):
+    return render(request, 'search.html')
 
 def predict(request):    
-    with open("trained.pickle","rb") as f:
+    with open("/home/rohan/Desktop/biet_hacks/biethackathon/trained.pickle","rb") as f:
         randomclassifier = pickle.load(f)
-    with open("vectorizer.pickle","rb") as t:
+    with open("/home/rohan/Desktop/biet_hacks/biethackathon/vectroizer.pickle","rb") as t:
         vectorizer = pickle.load(t)
     if request.method == "POST":
         text = request.POST.get('text')
         print(text)
         print("The datastypeof text is",type(text))
-        cleared_text = text.replace(r'[?.,!/]',"").lower()
-        text_num  = vectorizer.transform([cleared_text])
-        prediction = randomclassifier.predict(text_num)
+        
+        #here we will scrape the data from the web
+        data = scrapper(text)
+        #data = "The stocks are getting so damn high"
 
-        context = {'predict':prediction,'text':text}
+        cleared_text = data.replace(r'[?.,!/]',"").lower()
+        text_num  = vectorizer.transform([cleared_text])
+        prediction = bool(randomclassifier.predict(text_num)[0])
+        print(prediction)
+        context = {'predict':prediction,'text':text, 'News' : data}
         return render(request,'predict_post.html',context)
     else:
-        return render(request,'predict_pre.html')     
+        return render(request,'predict_pre.html')
